c2bf15d3924ab72dad23cffdc450b6c289f6f67c
Dave DeLong <davedelong@me.com>
2014-07-12 08:22:59 -0700

    Updating copyright date.

--- Unit Tests/UnitTests.m
+++ Unit Tests/UnitTests.m
@@ -2,7 +2,7 @@
 //  UnitTests.m
 //  CHCSVParser
 /**
- Copyright (c) 2010 Dave DeLong
+ Copyright (c) 2014 Dave DeLong
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
