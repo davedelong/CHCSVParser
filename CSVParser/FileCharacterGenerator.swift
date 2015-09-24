f6d564a46126816cb3809986fd9214bf173dc793
Dave DeLong <davedelong@me.com>
2015-09-24 10:25:02 -0600

    allow the page size and load thresholds to be customizable

--- CSVParser/FileCharacterGenerator.swift
+++ CSVParser/FileCharacterGenerator.swift
@@ -8,24 +8,34 @@
 
 import Foundation
 
-private let PageSize = 4096
-private let LoadMoreThreshold = 1024
+private let DefaultPageSize = 4096
+private let DefaultLoadMoreThreshold = 1024
 
 public class FileCharacterGenerator: GeneratorType {
     public typealias Element = Character
     
-    let input: NSInputStream
-    let bom: NSData
-    let encoding: NSStringEncoding
+    private let input: NSInputStream
+    private let bom: NSData
+    private let encoding: NSStringEncoding
     
-    var pendingByteBuffer = NSMutableData()
-    var characters = Array<Character>()
+    private let pageSize: Int
+    private let loadMoreThreshold: Int
+    private var pendingByteBuffer = NSMutableData()
+    private var characters = Array<Character>()
     
-    public init(file: NSURL, encoding: NSStringEncoding = NSMacOSRomanStringEncoding) {
+    public init(file: NSURL, encoding: NSStringEncoding = NSMacOSRomanStringEncoding, pageSize: Int = 4096, loadThreshold: Int = 1024) {
+        
         self.input = NSInputStream(URL: file) ?? NSInputStream()
         self.bom = encoding.bom
         self.encoding = encoding
-        self.characters.reserveCapacity(PageSize + LoadMoreThreshold)
+        
+        let page = pageSize > 0 ? pageSize : DefaultPageSize
+        let threshold = loadThreshold > 0 ? loadThreshold : DefaultLoadMoreThreshold
+        
+        self.pageSize = max(page, threshold)
+        self.loadMoreThreshold = min(page, threshold)
+        
+        self.characters.reserveCapacity(self.pageSize + self.loadMoreThreshold)
     }
     
     deinit {
@@ -45,13 +55,13 @@ public class FileCharacterGenerator: GeneratorType {
         
         if input.streamStatus == .AtEnd || input.streamStatus == .Closed || input.streamStatus == .Error { return }
         
-        // if we have at least 1024 characters, don't read more
-        guard characters.count < LoadMoreThreshold else { return }
+        // we only want to try to load more if we have fewer than 1024 characters
+        guard characters.count < loadMoreThreshold else { return }
         guard input.hasBytesAvailable else { return }
         
-        var buffer = Array<UInt8>(count: PageSize, repeatedValue: 0)
+        var buffer = Array<UInt8>(count: pageSize, repeatedValue: 0)
         
-        let bytesRead = input.read(&buffer, maxLength: PageSize)
+        let bytesRead = input.read(&buffer, maxLength: pageSize)
         pendingByteBuffer.appendBytes(buffer, length: bytesRead)
         
         guard pendingByteBuffer.length > 0 else { return }
