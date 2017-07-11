21b7b4ba843a9af374d3d296a763ba990a22cfe7
Toland Hon <thon@opentable.com>
2017-07-11 14:44:38 -0700

    add iOS target

---
+++ CHCSVParser-iOS/CHCSVParser-iOS.h
@@ -0,0 +1,19 @@
+//
+//  CHCSVParser-iOS.h
+//  CHCSVParser-iOS
+//
+//  Created by Toland Hon on 7/11/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHCSVParser-iOS.
+FOUNDATION_EXPORT double CHCSVParser_iOSVersionNumber;
+
+//! Project version string for CHCSVParser-iOS.
+FOUNDATION_EXPORT const unsigned char CHCSVParser_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHCSVParser_iOS/PublicHeader.h>
+
+
